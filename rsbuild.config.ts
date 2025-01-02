9d3bbbefe06148ca2600271203b9c049340a8867
Vinit <vinit1906@gmail.com>
2025-01-02 21:28:53 +0100

    chore: remove all env variables from rspack config
    no longer using these, see PR #4

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,9 +3,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
-  tools: {
-    rspack: {
-      plugins: [new rspack.EnvironmentPlugin(['NODE_ENV', 'RELAY_URL', 'MANAGER_API_BASE_URL'])]
-    }
-  }
 });
