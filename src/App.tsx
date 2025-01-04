72d732d497360f6264d2cad4c83256e5ea494619
Vinit <vinit1906@gmail.com>
2025-01-04 16:38:53 +0100

    chore: prevent nostr extension from triggering before app is initialized
    The extension would ask for permissions for event signinng before the base url
    was set. This is due to the fact that the app initialization happened inside
    one of the components of the application (Metadata), so the whole component had
    to load to render InitializeApp.
    
    This does not make sense as InitializeApp was supposed to take in the input
    that's required for initializing the whole application. Loading this component
    after part of the app is initialized and inside of another component is wrong.
    
    In this PR, we are improving things by moving this component to a higher level
    in the main App component. This way we can also hide the navigation and prevent
    rendering of outlets in case someone tries to navigate manually through the browser
    navigation bar.

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,63 @@ import { Outlet } from "react-router";
 import './App.css';
 import 'milligram/dist/milligram.min.css';
 
+import { useState } from 'react';
+import { UrlContext } from './UrlContext';
+import { useUrlStore } from './use-url-store';
 
 const App = () => {
-  return (
-    <div className="content">
-      <div className="navbar">
-        <div className="navbar-left">
-          <h2 className="title">Relay Management Panel</h2>
-        </div>
-        <div className="navbar-right">
-          <Link to="/">Home</Link>
-          <Link to="/pubkeys">Pubkeys</Link>
-          <Link to="/kinds">Kinds</Link>
-          <Link to="/blossom">Blossom</Link>
-        </div>
-      </div>
-      <Outlet />
-    </div>
-  );
+	const { isStoreInit, api, handleSetApi, resetStore } = useUrlStore();
+
+	return (
+		<UrlContext.Provider value={{ api, handleSetApi, resetStore }}>
+			<div className="content">
+				<div className="navbar">
+					<div className="navbar-left">
+						<h2 className="title">Relay Management Panel</h2>
+					</div>
+					{isStoreInit ?
+						<div className="navbar-right">
+							<Link to="/">Home</Link>
+							<Link to="/pubkeys">Pubkeys</Link>
+							<Link to="/kinds">Kinds</Link>
+							<Link to="/blossom">Blossom</Link>
+						</div> : null}
+				</div>
+
+				{isStoreInit ? <Outlet /> : <InitializeApp handleSetApi={handleSetApi} />}
+			</div>
+		</UrlContext.Provider>
+	);
 };
 
 export default App;
+
+
+function InitializeApp({ handleSetApi }: { handleSetApi: (baseUrl: string, endpoint: string) => void }) {
+	const [baseUrl, setBaseUrl] = useState('');
+	const [endpoint, setEndpoint] = useState('');
+
+	return (
+		<div>
+			<p>To get started, we need two things entered below.</p> 
+			<p>First, please enter "Relay base url", which is the http(s)
+				(not wss!) URL of your relay without the endpoint path. For example, https://myrelay.com 
+			</p>
+			<p>
+				Second, please enter the "Relay management endpoint path", so we know where to send NIP-86 
+				requests. For example, /admin/manage. 
+				Both values combined it would look like https://myrelay.com/manage
+			</p>
+			Relay base url: <input type="text" value={baseUrl}
+				onChange={(e) => setBaseUrl(e.currentTarget.value)} placeholder="Example, http://myrelay.com"
+			/>
+			Relay management endpoint: <input type="text" value={endpoint}
+				onChange={(e) => setEndpoint(e.currentTarget.value)} placeholder="Example, /admin/manage"
+			/>
+			<button onClick={() => {
+				if (!baseUrl || !endpoint) return;
+				handleSetApi(baseUrl, endpoint)
+			}}>Save</button>
+		</div>
+	)
+}
